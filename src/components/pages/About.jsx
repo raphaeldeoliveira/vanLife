4868decf587995f4fce1dca011f181f5056ca96a
raphaeldeoliveira <rafaolive3003@gmail.com>
2024-01-05 19:03:17 -0300

    correção do ultimo erro do ESLint para deploy

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
 
     return (
         <div className="about">
-            <img className="about__banner-img" src={bannerImage} />
+            <img className="about__banner-img" src={bannerImage} alt=""/>
             <h1>Don't squeeze in a sedan when you could relax in a van.</h1>
             <p>Our mission is to enliven your road trop with the perfect travel van rental. Our vans are recertified before wach trip to ensure yout travel plans can go off whithout a hitch.</p>
             <p className="about__second-p">Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
